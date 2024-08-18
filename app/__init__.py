21ce0ac1409a1b76723ecf8976f379319b4bddfc
Tom Camp <tomcamp@protonmail.ch>
2024-08-17 21:38:10 -0400

    Creating project scan.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,20 @@ import os
 from flask import Flask, render_template
 
 from app.projects.routes import project_bp
+from app.utils.helpers import get_projects
+
+
+def intro_text():
+    return """
+    The SSP Toolkit is used to create System Security Plans using components
+    and templates. Components, appendices, and frontmatter can be added to a
+    project and edited. Key YAML files containing variables that can be used
+    to populate the variables in the templates can be edited and created.
+
+    You can create a Project here or clone a git repository of Project files
+    in the project_data directory. Any directory in the project_directory that
+    includes a project.yaml file will be accessible in the Toolkit.
+    """
 
 
 def create_app(test_config=None):
@@ -24,7 +38,12 @@ def create_app(test_config=None):
     app.register_blueprint(project_bp)
 
     @app.route("/")
-    def hello():
-        return render_template("index.html")
+    def index():
+        projects = get_projects()
+        page_data = {
+            "projects": projects if len(projects) <= 5 else projects[:5],
+            "intro": intro_text(),
+        }
+        return render_template("page/index.html", **page_data)
 
     return app
