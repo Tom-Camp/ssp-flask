b9b07f5a874b03fc93a9936a537c5976719c7503
Tom Camp <tomcamp@protonmail.ch>
2024-08-18 17:41:58 -0400

    Initial testing set up. (#23)

---
+++ tests/test_helpers.py
@@ -0,0 +1,33 @@
+from pathlib import Path, PosixPath
+
+from app.utils import helpers
+
+
+def test_machine_name():
+    machine_name = helpers.get_machine_name(name="t!e@s#t$ n%a^m&e*")
+    assert machine_name == "test_name"
+
+
+def test_load_yaml():
+    filepath = "tests/assets/test.yaml"
+    load_yaml = helpers.load_yaml(filepath)
+    assert type(load_yaml) is dict
+    assert load_yaml.get("name").get("title") == "Test YAMl"
+
+
+def test_write_yaml(client):
+    _ = client.get("/")
+    write_path = Path("tests/assets/write_test.yaml")
+    filepath = "tests/assets/test.yaml"
+    load_yaml = helpers.load_yaml(filepath)
+
+    helpers.write_yaml(write_path.as_posix(), load_yaml)
+
+    assert write_path.exists() is True
+    write_path.unlink()
+
+
+def test_scan_dir():
+    files = helpers.scan_dir("tests/assets/scanme")
+    assert len(files) == 4
+    assert isinstance(files[0], PosixPath)
