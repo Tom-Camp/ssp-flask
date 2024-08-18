b3fa37194fad71e08e52eefe385f66d04ad3e900
Tom Camp <tomcamp@protonmail.ch>
2024-08-18 15:51:24 -0400

    Updating add/remove for oc files. (#20)

--- app/__init__.py
+++ app/__init__.py
@@ -19,11 +19,21 @@ def intro_text():
     """
 
 
+def page_not_found(e):
+    return render_template("errors/404.html"), 404
+
+
+def internal_server_error(e):
+    return render_template("errors/500.html"), 500
+
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
     )
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, internal_server_error)
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
