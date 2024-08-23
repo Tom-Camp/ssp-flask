80a7667894550426cf45412b00c49e9b6ce7c872
Tom Camp <tomcamp@protonmail.ch>
2024-08-22 21:07:52 -0400

    Big refactoring (#28)
    * Refactoring
    
    * Refactoring.

--- app/projects/routes.py
+++ app/projects/routes.py
@@ -12,35 +12,56 @@ project_bp = Blueprint("project", __name__, url_prefix="/project")
 
 
 def get_destination_path(file: str) -> str:
-    return Path(*Path(file).parts[1:]).as_posix()
+    """
+    Given a library path and a Project machine_name, return a file path to use
+    to copy the library file into the Project.
+
+    :param file: str - the library file path
+    :return: str - the file path in the Project directory
+    """
+    return "/".join(Path(file).parts[1:])
 
 
-@project_bp.route("/list", methods=["GET"])
-def projects_list():
-    projects = get_projects()
-    return render_template("project/projects.html", projects=projects)
+def project_add_files(project_name: str, file_list: list) -> list:
+    """
+    Give a list of library files, add them to a Project.
+
+    :param project_name: str the Project machine_name
+    :param file_list: a list of library file paths as strings
+    :return: a list of Project file paths as strings
+    """
+    files: list = []
+    library = Library(project_machine_name=project_name)
+    for file in file_list:
+        files.append(library.copy(filepath=file))
+    return files
 
 
-@project_bp.route("/create", methods=["GET", "POST"])
-def projects_create():
-    form = ProjectForm()
-    if request.method == "POST":
-        if form.validate_on_submit():
-            project = Project(
-                name=form.name.data,
-                description=form.description.data,
-                maintainers=[name.get("maintainer") for name in form.maintainers.data],
-                project_dir="",
-            )
-            project.create()
-            return redirect("/project/list")
+def project_remove_files(project_name: str, file_list: list) -> list:
+    """
+    Give a list of library files, remove them to a Project.
 
-    return render_template("project/projects_form.html", form=form)
+    :param project_name: str the Project machine_name
+    :param file_list: a list of library file paths as strings
+    :return: a list of Project file paths as strings
+    """
+    files: list = []
+    library = Library(project_machine_name=project_name)
+    for file in file_list:
+        library.remove(
+            filepath=file,
+        )
+        files.append(file)
+    return files
 
 
-@project_bp.route("/<project_name>", methods=["GET"])
-def project_view(project_name: str):
-    page: dict = {}
+def load_project(project_name: str) -> Project:
+    """
+    Return a Project object.
+
+    :param project_name: str the Project machine_name
+    :return: Project
+    """
     project_file = load_yaml(
         Path("project_data")
         .joinpath(project_name)
@@ -48,57 +69,110 @@ def project_view(project_name: str):
         .with_suffix(".yaml")
         .as_posix()
     )
+    # Need to fix this.
     if project_file:
         project = Project(**project_file)
-        page = {"appendices": project.get_appendices(), "data": project.load()}
+    else:
+        project = Project()
+    return project
+
+
+@project_bp.route("/list", methods=["GET"])
+def project_list_page():
+    """
+    A page to list all the Projects.
+
+    :return: HTML template
+    """
+    projects = get_projects()
+    return render_template("project/project_list.html", projects=projects)
+
+
+@project_bp.route("/create", methods=["GET", "POST"])
+def project_create_page():
+    """
+    A page to create an SSP Toolkit Project
+
+    :return: HTML template
+    """
+    form = ProjectForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            project = Project(
+                name=form.name.data,
+                description=form.description.data,
+                maintainers=[name.get("maintainer") for name in form.maintainers.data],
+                project_dir=None,
+                opencontrol=None,
+                machine_name=None,
+            )
+            project.create()
+            return redirect("/project/list")
+
+    return render_template("project/project_form.html", form=form)
+
+
+@project_bp.route("/<project_name>", methods=["GET"])
+def project_view_page(project_name: str):
+    """
+    A view of an individual Project.
+
+    :param project_name: str - machine_name for the Project.
+    :return: HTML template
+    """
+    project = load_project(project_name=project_name)
+    page: dict = project.view()
     return render_template("project/project.html", **page)
 
 
-@project_bp.route("/<project_name>/add/<filetype>", methods=["GET", "POST"])
-def project_add_oc_files(project_name: str, filetype: str):
-    oc_key = filetype.lower()
-    if oc_key not in ["standards", "certifications"]:
+@project_bp.route("/<project_name>/add/<directory>", methods=["GET", "POST"])
+def project_files_add_page(project_name: str, directory: str):
+    """
+    A page to add OpenControl certifications and standards.
+
+    :param project_name: str - machine_name for the Project.
+    :param directory: str - either standards or certifications
+    :return: HTML template
+    """
+    source = directory.lower()
+    library = Library(project_machine_name=project_name)
+    directories = library.list_directories()
+
+    if source not in directories:
         abort(404)
 
-    project_path = Path("project_data").joinpath(project_name)
-    opencontrol = project_path.joinpath("opencontrol").with_suffix(".yaml")
+    files = library.list_files(directory=source)
+    project = load_project(project_name)
 
-    opencontrol_file = load_yaml(opencontrol.as_posix())
-    project_file = load_yaml(
-        project_path.joinpath("project").with_suffix(".yaml").as_posix()
-    )
-
-    oc_files: list = [
-        Path(oc_file).name for oc_file in opencontrol_file.get(oc_key, [])
-    ]
-    library = Library(project_path=project_path.as_posix())
-    files = Library(project_path=project_path.as_posix()).list_files(dirname=oc_key)
     data: dict = {
-        "filename": f"opencontrol.yaml {oc_key}",
         "project_name": project_name,
-        "name": project_file.get("name"),
+        "name": project.name,
         "file_list": files,
-        "oc_files": oc_files,
+        "project_files": project.get_project_files_by_directory(directory=directory),
     }
 
-    if request.method == "POST":
-        for file in request.form.getlist("files"):
-            filename = get_destination_path(file)
-            destination = filename
-            library.copy(
-                filename=filename,
-                dest=destination,
-            )
-            opencontrol_file[oc_key].append(destination)
-            write_yaml(
-                filename=opencontrol.as_posix(),
-                data=opencontrol_file,
-            )
+    # if request.method == "POST":
+    #     file_list: list = [file for file in request.form.getlist("files")]
+    #     new_files = project_add_files(project_name=project_name, file_list=file_list)
+    #     opencontrol_file[oc_key] = new_files
+    #     write_yaml(
+    #         filename=opencontrol.as_posix(),
+    #         data=opencontrol_file,
+    #     )
+
     return render_template("project/project_add_files_form.html", **data)
 
 
 @project_bp.route("/<project_name>/delete/<filetype>/<filename>", methods=["GET"])
-def project_delete_oc_files(project_name: str, filetype: str, filename: str):
+def project_files_delete_page(project_name: str, filetype: str, filename: str):
+    """
+    A page to remove OpenControl certifications and standards.
+
+    :param project_name: str - machine_name for the Project.
+    :param filetype: str - either standards or certifications
+    :param filename: str - the filename to remove
+    :return: HTML template
+    """
     oc_key = filetype.lower()
     if oc_key not in ["standards", "certifications"]:
         abort(404)
@@ -114,25 +188,6 @@ def project_delete_oc_files(project_name: str, filetype: str, filename: str):
         Path(oc_file).name: Path(oc_file).as_posix()
         for oc_file in opencontrol_file.get(oc_key, [])
     }
-    library = Library(
-        project_path=Path("project_data").joinpath(project_name).as_posix()
-    )
-    library.remove(filename=oc_files[filename])
     opencontrol_file["standards"].remove(oc_files[filename])
     write_yaml(filename=opencontrol.as_posix(), data=opencontrol_file)
     return redirect(f"/project/{project_name}/add/{oc_key}")
-
-
-@project_bp.route("/<project_name>/add/templates", methods=["GET", "POST"])
-def project_add_templates(project_name: str):
-    library = Library(
-        project_path=Path("project_data")
-        .joinpath(project_name)
-        .joinpath("templates")
-        .as_posix()
-    )
-    library.copy_dir(
-        directory="appendices",
-        dest="templates/appendices",
-    )
-    return redirect(f"/project/{project_name}")
--- app/utils/library.py
+++ app/utils/library.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 from dataclasses import dataclass
 from pathlib import Path
@@ -7,70 +8,89 @@ from flask import flash
 
 @dataclass
 class Library:
-    project_path: str
+    project_machine_name: str
 
     @staticmethod
-    def list_files(dirname: str) -> list:
+    def list_files(directory: str) -> list:
         files: list = []
         try:
             files = [
                 (file.name, "/".join(file.parts[1:]))
-                for file in Path("library").joinpath(dirname).glob("*")
+                for file in Path("library").joinpath(directory).iterdir()
+                if file.is_file()
             ]
         except FileNotFoundError:
-            flash(f"{dirname} was not found.")
+            flash(f"{directory} was not found.")
         except PermissionError:
-            print(f"Permission denied. Cannot read {dirname}.")
+            print(f"Permission denied. Cannot read {directory}.")
         except Exception as e:
             print(f"An error occurred: {e}")
         finally:
             pass
         return files
 
-    def copy(self, filename: str, dest: str | None):
-        source = Path("library").joinpath(filename)
-        destination = (
-            Path(self.project_path).joinpath(dest) if dest else Path(self.project_path)
-        )
-
+    @staticmethod
+    def list_directories(directory: str = ""):
+        dirs: list = []
         try:
-            shutil.copy(src=source, dst=destination)
+            dirs = [
+                file.name
+                for file in Path("library").joinpath(directory).iterdir()
+                if file.is_dir()
+            ]
         except FileNotFoundError:
-            flash(f"{source.as_posix()} was not found.")
+            flash(f"{directory} was not found.")
         except PermissionError:
-            print(
-                f"Permission denied. Cannot copy {source.as_posix()} to {destination.as_posix()}."
-            )
+            print(f"Permission denied. Cannot read {directory}.")
         except Exception as e:
             print(f"An error occurred: {e}")
         finally:
-            pass
+            return dirs
 
-    def copy_dir(self, directory: str, dest: str | None):
-        source = Path("library").joinpath(directory)
-        destination = (
-            Path(self.project_path).joinpath(dest) if dest else Path(self.project_path)
-        )
+    @staticmethod
+    def get_directory_tree(directory: str = "") -> dict:
+        directory_dict: dict = {}
+        for root, dirs, files in os.walk(
+            Path("library").joinpath(directory).as_posix()
+        ):
+            current_dict = directory_dict
+            for part in os.path.relpath(root, directory).split(os.sep):
+                if part != ".":
+                    current_dict = current_dict.setdefault(part, {})
+            for file in files:
+                current_dict[file] = None
+        return directory_dict
+
+    def copy(self, filepath: str) -> str:
+        source = Path("library").joinpath(filepath).as_posix()
+        destination = self._get_destination(filepath=filepath)
 
         try:
             shutil.copytree(src=source, dst=destination, dirs_exist_ok=True)
             flash(f"{source} copied to {destination}", "success")
         except FileNotFoundError:
-            flash(f"{source.as_posix()} was not found.")
+            flash(f"{source} was not found.")
         except PermissionError:
-            print(
-                f"Permission denied. Cannot copy {source.as_posix()} to {destination.as_posix()}."
-            )
+            print(f"Permission denied. Cannot copy {source} to {destination}.")
         except Exception as e:
             print(f"An error occurred: {e}")
         finally:
-            pass
+            return destination
 
-    def remove(self, filename: str):
+    def remove(self, filepath: str):
+        destination = self._get_destination(filepath=filepath)
         try:
-            Path(self.project_path).joinpath(filename).unlink(missing_ok=False)
-            flash(f"File {filename} removed from project.", "success")
+            Path(destination).unlink(missing_ok=False)
+            flash(f"File {destination} removed from project.", "success")
         except FileNotFoundError:
-            flash(f"The file {filename} was not found.", "error")
+            flash(f"The file {destination} was not found.", "error")
         finally:
             pass
+
+    def _get_destination(self, filepath: str) -> str:
+        return (
+            Path("project_data")
+            .joinpath(self.project_machine_name)
+            .joinpath(filepath)
+            .as_posix()
+        )
