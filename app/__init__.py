d695daa6a3d162e13be72bafa3c41853796cafd4
Tom Camp <tomcamp@protonmail.ch>
2024-08-27 07:25:30 -0400

    Setting up basic logging.

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import logging.config
 import os
 
 from flask import Flask, render_template
@@ -6,21 +7,13 @@ from app.pages.routes import page_bp
 from app.projects.routes import project_bp
 
 
-def page_not_found(e):
-    return render_template("errors/404.html"), 404
-
-
-def internal_server_error(e):
-    return render_template("errors/500.html"), 500
-
-
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
     )
-    app.register_error_handler(404, page_not_found)
-    app.register_error_handler(500, internal_server_error)
+
+    logging.config.fileConfig("app/logging.conf")
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
@@ -35,4 +28,19 @@ def create_app(test_config=None):
     app.register_blueprint(page_bp)
     app.register_blueprint(project_bp)
 
+    @app.errorhandler(Exception)
+    def handle_exception(e):
+        app.logger.error(f"An error occurred: {e}")
+        return render_template("errors/500.html"), 500
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        app.logger.error(f"404 error: {error}")
+        return render_template("errors/404.html"), 404
+
     return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
