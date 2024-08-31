ee5432e358ef95ebcede794664aba836ed798818
Tom Camp <tomcamp@protonmail.ch>
2024-08-31 14:16:55 -0400

    Project redo

---
+++ app/projects/file_manager.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from flask import flash
+from pydantic import BaseModel, model_validator
+
+from config import config
+
+ROOT_DIR = config.get("ROOT_DIR", Path())
+
+
+class FileManager(BaseModel):
+    project_machine_name: str
+    template_path: Path = None  # type: ignore
+    project_path: Path = None  # type: ignore
+
+    @model_validator(mode="after")
+    def set_template_path(cls, model):
+        model.project_path = ROOT_DIR.joinpath("project_data").joinpath(
+            model.project_machine_name
+        )
+        if model.project_path.is_dir():
+            model.template_path = model.project_path.joinpath("templates")
+
+    def get_files(self) -> list:
+        return [file.as_posix() for file in self.template_path.rglob("*")]
+
+    def get_files_by_directory(self, directory: str) -> list:
+        return [file.name for file in self.template_path.joinpath(directory).glob("*")]
+
+    def get_copy_destination(self, filepath: str) -> str:
+        return self.template_path.joinpath(filepath).as_posix()
+
+    def remove_file(self, source_path: str):
+        source = self.project_path.joinpath(source_path)
+        destination = self.project_path.joinpath("trash").joinpath(source)
+        destination.parent.mkdir(parents=True, exist_ok=True)
+        try:
+            source.rename(destination)
+            flash(
+                message=f"{source_path} moved to Project trash", category="is-success"
+            )
+        except FileExistsError:
+            flash(f"Error writing file to {destination}")
+        finally:
+            pass
---
+++ app/toolkit/opencontrol.py
@@ -0,0 +1,139 @@
+from enum import Enum
+from pathlib import Path
+from typing import List, Optional, Set
+
+import rtyaml
+from pydantic import BaseModel, Field
+
+from config import config
+
+ROOT_DIR = config.get("ROOT_DIR", Path())
+
+OPENCONTROL_SCHEMA_VERSION = "1.0.0"
+COMPONENT_SCHEMA_VERSION = "3.1.0"
+
+
+class OpenControlElement(BaseModel):
+    @staticmethod
+    def new_relative_path(self):
+        assert False
+
+    def storage_path(self, root_dir=None):
+        if hasattr(self, "_file"):
+            p = Path(self._file)
+        else:
+            p = self.new_relative_path()
+        if root_dir:
+            return root_dir / p
+        else:
+            return p
+
+
+class Reference(OpenControlElement):
+    name: str
+    path: str  # TODO (url?)
+    type: str  # TODO (enum)
+
+
+class Statement(OpenControlElement):
+    text: str
+    key: Optional[str]
+
+
+class Parameter(OpenControlElement):
+    key: str
+    text: str
+
+
+class ImplementationStatusEnum(str, Enum):
+    partial = "partial"
+    complete = "complete"
+    planned = "planned"
+    none = "none"
+
+
+class CoveredBy(OpenControlElement):
+    verification_key: str
+    system_key: Optional[str]
+    component_key: Optional[str]
+
+
+class Control(OpenControlElement):
+    control_key: str
+    standard_key: str
+    covered_by: Optional[List[CoveredBy]]
+    narrative: Optional[List[Statement]]
+    references: Optional[List[Reference]]
+    implementation_statuses: Optional[Set[ImplementationStatusEnum]]
+    control_origins: Optional[List[str]]
+    parameters: Optional[List[Parameter]]
+
+
+class ComponentFile(OpenControlElement):
+    family: str
+    documentation_complete: bool
+    satisfies: Optional[List[Control]]
+
+
+class Component(OpenControlElement):
+    name: str
+    schema_version: str = COMPONENT_SCHEMA_VERSION
+    satisfies: List[str]
+
+    def update(self, action: str, component: str):
+        component_path = f"./{component}"
+        if action == "add" and component_path not in self.satisfies:
+            self.satisfies.append(component_path)
+        elif action == "remove" and component_path in self.satisfies:
+            self.satisfies.remove(component_path)
+
+    def write(self, component: str):
+        component_path = (
+            ROOT_DIR.joinpath("templates")  # type: ignore
+            .joinpath("components")  # type: ignore
+            .joinpath(component)  # type: ignore
+            .joinpath("component")
+            .with_suffix(".yaml")  # type: ignore
+        )
+        with component_path.open("w+") as oc:
+            oc.write(rtyaml.dump(self.model_dump()))
+
+
+class Metadata(BaseModel):
+    description: str
+    maintainers: Optional[List[str]] = Field(default=[])
+
+
+class OpenControl(BaseModel):
+    schema_version: str = OPENCONTROL_SCHEMA_VERSION
+    name: str
+    metadata: Optional[Metadata]
+    components: List[str] = Field(default=[])
+    certifications: List[str] = Field(default=[])
+    standards: List[str] = Field(default=[])
+
+    def __init__(self, name: str, description: str, maintainers: list):
+        metadata = Metadata(
+            description=description,
+            maintainers=maintainers,
+        )
+        super().__init__(
+            name=name,
+            metadata=metadata,
+        )
+
+    def update(self, key: str, action: str, component: str):
+        component_path = f"./{component}"
+        field = getattr(self, key)
+        if action == "add" and component_path not in field:
+            field.append(component_path)
+        elif action == "remove" and component_path in field:
+            field.remove(component_path)
+        setattr(self, key, field)
+
+    def write(self, project_path: str):
+        opencontrol_path = (
+            ROOT_DIR.joinpath(project_path).joinpath("opencontrol").with_suffix(".yaml")  # type: ignore
+        )
+        with opencontrol_path.open("w+") as oc:
+            oc.write(rtyaml.dump(self.model_dump()))
